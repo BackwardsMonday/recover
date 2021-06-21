d9631f189a3efff004ab1d4fafbdb808f6836517
Daymon Wright <wrightdaymon@gmail.com>
2021-06-21 11:00:47 -0600

    initail commit

---
+++ recover.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(int argc, char *argv[])
+{
+    char *boldRed = "\033[1;31m";
+    if (argc != 2)
+    {
+        printf("Error");
+        return 1;
+    }
+    
+}
